ca6a0cfd612fa6d4c5c9830c51317c467c4f148c
turbojax07 <turbojax07@gmail.com>
2024-03-14 23:55:05 -0400

    Removing some GUI remnants

--- src/main/java/control/lifx/Main.java
+++ src/main/java/control/lifx/Main.java
@@ -12,17 +12,6 @@ public class Main {
       ReceiveMessages receiveMessages = new ReceiveMessages();
       receiveMessages.start();
 
-      // Collects Lifx bulb IPs.
-      System.out.println("Getting ips...");
-      ArrayList<String> ips = IPGrabber.getIps();
-
-      // Creates an object for each of the found lights
-      ArrayList<Light> lights = new ArrayList<Light>();
-      ips.forEach((ip) -> {
-         System.out.println(ip);
-         lights.add(new Light(ip, Color.red));
-      });
-
       System.out.println("Initializing the text interface...");
       
       
